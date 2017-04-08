f0406971190b386df6acebb7c1a4f99bda26d4d6
Adam Pavlidis <adampavlidis@gmail.com>
2017-04-08 21:49:34 +0300

    Refactored /api/netdevices and /api/netdevices/<id> with flask-restful

--- app/views.py
+++ app/views.py
@@ -1,12 +1,14 @@
 from app import app
 from app import db
-from flask import render_template
+from flask_restful import Api, Resource, reqparse, fields, marshal
 from werkzeug.exceptions import *
 import re
 from models import NetDevice, NetDevicePorts
 
 from datetime import datetime, timedelta
 from flask import jsonify, abort, json, request
+
+api = Api(app)
 
 # Params for each model that are expected
 query_params = {
@@ -110,52 +112,6 @@ def get_patchpanels_query():
     # return jsonify({'episodes': })
 
 
-@app.route('/api/netdevices', strict_slashes=False, methods=['GET', 'POST'])
-def get_netdevices():
-    if request.method == 'GET':
-        devices = NetDevice.query.all()
-        results = []
-
-        return jsonify({"result": [device.serialize for device in devices]})
-
-    if request.method == 'POST':
-
-        # Validate JSON
-        try:
-            input_json = request.json
-        except BadRequest, e:
-            msg = "ERROR: Invalid JSON"
-            return jsonify({'error': msg}), 400
-
-        # return jsonify({"result": input_json})
-
-        # Check JSON fields
-
-        if input_json:
-            # TODO perform the checks for all fields
-            new_nd = NetDevice()
-            for param in query_params['netdevices']:
-                if (param in input_json) and (type(input_json[param]) != unicode):
-                    return jsonify({'error': 'Bad Request on field %s' % param}), 400
-                elif param not in input_json:
-                    return jsonify({'error': 'Bad JSON field %s is needed' % param}), 400
-                else:
-                    new_nd.__setattr__(param, input_json[param])
-
-            db.session.add(new_nd)
-            db.session.commit()
-            return jsonify({'result': input_json})
-
-
-@app.route('/api/netdevices/<int:device_id>', strict_slashes=False)
-def get_netdevice(device_id):
-    try:
-        inputJSON = request.json
-    except BadRequest, e:
-        msg = "ERROR: Invalid JSON"
-        return jsonify({'error': msg}), 400
-
-
 @app.route('/api/netdevices/query', strict_slashes=False)
 def get_netdevices_query():
     try:
@@ -225,3 +181,81 @@ def get_netdevice_ports(device_id):
             return jsonify({"result": results})
 
         return jsonify({'error': "no JSON"}), 400
+
+
+class NetDeviceAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('vendor', type=str, location='json')
+        self.reqparse.add_argument('device_type', type=str, location='json')
+        self.reqparse.add_argument('serial_number', type=str, location='json')
+        self.reqparse.add_argument('management_ip', type=str, location='json')
+        super(NetDeviceAPI, self).__init__()
+
+    def get(self, device_id):
+        device = NetDevice.query.get(device_id)
+        if not device:
+            return {'error': 'No such item'}, 404
+
+        return {'result': device.serialize}, 200
+
+    def put(self, device_id):
+        device = NetDevice.query.get(device_id)
+        if not device:
+            return {'error': 'No such item'}, 404
+
+        args = self.reqparse.parse_args(strict=True)
+
+        for k, v in args.iteritems():
+            if v and (device.__getattribute__(k) != v):
+                device.__setattr__(k, v)
+
+        db.session.commit()
+
+        return {'results': device.serialize}, 200
+
+    def delete(self, device_id):
+        device = NetDevice.query.get(device_id)
+        if not device:
+            return {'error': 'No such item'}, 404
+
+        db.session.delete(device)
+        db.session.commit()
+
+        return {'deleted': device.serialize}, 200
+
+
+class NetDeviceListAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('vendor', type=str, location='json', required=True)
+        self.reqparse.add_argument('device_type', type=str, location='json', required=True)
+        self.reqparse.add_argument('serial_number', type=str, location='json', required=True)
+        self.reqparse.add_argument('management_ip', type=str, location='json', required=True)
+        super(NetDeviceListAPI, self).__init__()
+
+    def get(self):
+        devices = NetDevice.query.all()
+        return {"result": [device.serialize for device in devices]}
+
+    def post(self):
+        args = self.reqparse.parse_args(strict=True)
+        new_nd = NetDevice()
+
+        for k, v in args.iteritems():
+            new_nd.__setattr__(k, v)
+
+        db.session.add(new_nd)
+        db.session.commit()
+
+        inserted = NetDevice.query.order_by('-id').first()
+
+        if not inserted:
+            return {'error': 'No such item'}, 404
+
+        return {'results': inserted.serialize}, 200
+
+
+api.add_resource(NetDeviceAPI, '/api/netdevices/<int:device_id>')
+api.add_resource(NetDeviceListAPI, '/api/netdevices')
+
